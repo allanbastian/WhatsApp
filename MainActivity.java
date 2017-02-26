8842746acc0d65a9f7cd25012722b14f11dbe143
allanbastian <allanp.bastian2016@vitstudent.ac.in>
2017-02-26 22:48:12 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
@@ -19,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    String[] names = {"A", "B", "C", "D", "E", "F"};
+    String[] statuses = {"Hi", "Whats up?", "Hi there!", "Hello", "No calls", "Busy"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.viewpager);
         tabLayout.setupWithViewPager(viewPager);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        adapter = new RecyclerAdapter(names, statuses);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
     }
 
     public void setupViewPager(ViewPager upViewPager) {
